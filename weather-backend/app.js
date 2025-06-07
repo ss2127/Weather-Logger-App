183b1280b275b15e1a642267b37fe8040fa4ecdc
Samuel Ly <samuel.lyee01@gmail.com>
2025-06-07 09:32:31 -0400

    updated changes

--- weather-backend/app.js
+++ weather-backend/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const weatherLogRoutes = require('./routes/weatherRoutes');
-// const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const app = express();
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 
-app.use(express.json());
+const weatherLogRoutes = require('./routes/weatherRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+// ========== Security Middleware ==========
+app.use(express.json({ limit: '10kb' }));
 app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
+// ========== Routes ==========
 app.use('/api/v1/weatherLogs', weatherLogRoutes);
-// app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
+
 module.exports = app;
