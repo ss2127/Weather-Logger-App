735e8fb58dacc21e318a788487fdea37785c05c1
Samuel Ly <samuel.lyee01@gmail.com>
2025-06-07 10:42:01 -0400

    remove security malwares

--- weather-backend/app.js
+++ weather-backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 // const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
@@ -12,9 +12,9 @@ const app = express();
 // ========== Security Middleware ==========
 app.use(express.json({ limit: '10kb' }));
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
 // app.use(xss());
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 
 // ========== Routes ==========
 app.use('/api/v1/weatherLogs', weatherLogRoutes);
